43e726dbd356606e81f6259e2a1f66378c5da985
RawJSON <jayancobky567@gmail.com>
2025-12-03 16:12:19 -0500

    feat(ux): switch swipe actions to use high-contrast emojis (0Fluff)

--- script.js
+++ script.js
@@ -258,17 +258,9 @@ function render() {
     list.innerHTML = '';
     
     // SVG Icons (minimal, efficient, "0Fluff")
-    // NEW: Higher-contrast, visually distinct icons
-    const deleteIcon = `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-6 h-6">
-            <path d="M3 5a1 1 0 011-1h16a1 1 0 011 1v1a1 1 0 01-1 1H4a1 1 0 01-1-1V5zm3 3h12v11a2 2 0 01-2 2H8a2 2 0 01-2-2V8zm5 1.5a.5.5 0 00-1 0v8a.5.5 0 001 0v-8zm4 0a.5.5 0 00-1 0v8a.5.5 0 001 0v-8z"/>
-        </svg>
-    `;
-    const favoriteIcon = `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-6 h-6">
-            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-        </svg>
-    `;
+    // FIX: Switched to simple, high-contrast emojis
+    const deleteIcon = `❌`;
+    const favoriteIcon = `♥️`;
 
     // Sort: Favorites first, then Newest
     const sorted = [...recipes].sort((a, b) => {
